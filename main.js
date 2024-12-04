98663d4d7b3588037a506644dc9159ab614eccd4
Yuriy Murachov <dahakayuriy@gmail.com>
2024-12-04 09:26:33 +0200

    homework3

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+const str1 = prompt("Enter the first line:");
+const str2 = prompt("Enter the second line:");
+const str3 = prompt("Enter the third line:");
+
+alert("Result: " + str3 + ", " + str1 + ", " + str2);
