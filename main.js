515a65ad3429fcceb7bef92494c2944a37bcc76d
Yuriy Murachov <dahakayuriy@gmail.com>
2024-12-04 09:38:34 +0200

    changed homework

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ const str1 = prompt("Enter the first line:");
 const str2 = prompt("Enter the second line:");
 const str3 = prompt("Enter the third line:");
 
-alert("Result: " + str3 + ", " + str1 + ", " + str2);
+alert(
+  `Result: ${str1 || "the first line is not entered"}, ${
+    str2 || "the second line is not entered"
+  }, ${str3 || "the third line is not entered"}`
+);
